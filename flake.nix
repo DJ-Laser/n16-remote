7b5b111c04716ce94cdf7138445d668bdeb22513
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-02-09 10:34:16 -0500

    add direnv

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    fenix,
+  } @ inputs: let
+    system = "x86_64-linux";
+    overlays = [(fenix.overlays.default)];
+    pkgs = import nixpkgs {inherit system overlays;};
+
+    microcontrollerTarget = "thumbv6m-none-eabi";
+    rustToolchain = with fenix.packages.${system};
+      combine [
+        stable.cargo
+        stable.rustc
+        targets.${microcontrollerTarget}.latest.rust-std
+      ];
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [alejandra rustToolchain elf2uf2-rs kicad];
+      RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/src";
+    };
+  };
+}
