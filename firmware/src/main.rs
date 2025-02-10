156104ccfc377fea92e95db17c92c3af0fe061f1
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-02-09 20:48:44 -0500

    the leds on the xiao are linked to gnd

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -11,15 +11,15 @@ use {defmt_rtt as _, panic_probe as _};
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_25, Level::Low);
+    let mut led = Output::new(p.PIN_25, Level::High);
 
     loop {
         info!("led on!");
-        led.set_high();
+        led.set_low();
         Timer::after_secs(1).await;
 
         info!("led off!");
-        led.set_low();
+        led.set_high();
         Timer::after_secs(1).await;
     }
 }
